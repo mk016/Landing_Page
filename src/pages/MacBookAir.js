5b6d362ce543fdc84de65e1086fd28bb0447c338
Mahendra Kumawat <mahendrakumawat80224@gmail.com>
2024-03-21 04:44:43 +0530

    first commit

---
+++ src/pages/MacBookAir.js
@@ -0,0 +1,41 @@
+import "./MacBookAir.css";
+
+const MacBookAir = () => {
+  return (
+    <div className="macbook-air-11">
+      <div className="logo-group">
+        <b className="logo1">LOGO</b>
+        <div className="home-group">
+          <b className="home1">Home</b>
+          <div className="illustrations1">Illustrations</div>
+          <div className="contact1">Contact</div>
+        </div>
+        <div className="sign-up-frame">
+          <b className="sign-up2">Sign up</b>
+        </div>
+        <div className="sign-in-container">
+          <b className="sign-up2">Sign in</b>
+        </div>
+      </div>
+      <img className="saly-1-icon1" alt="" src="/saly1@2x.png" />
+      <b className="make-your-business-container1">
+        <p className="make-your-business1">
+          <span className="make-your1">Make Your </span>
+          <span className="business1">Business</span>
+          <span> </span>
+        </p>
+        <p className="make-your-business1">Stand out!</p>
+      </b>
+      <input className="email-container" type="text" placeholder="Email address" />
+      <div className="frame-div">
+        <b className="sign-up2">Sign up</b>
+      </div>
+      <div className="we-provide-the1">
+        This is My First React landing Page
+      </div>
+      <div className="mk0161">@mk016</div>
+    </div>
+  );
+};
+
+export default MacBookAir;
